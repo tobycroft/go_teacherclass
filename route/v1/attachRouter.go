b2329d53f0800ef9b3b4b404d96b7a8a1e4b62b7
火线兔 <silverhawk@foxmail.com>
2022-10-19 17:10:33 +0800

    TuuzMac

--- route/v1/attachRouter.go
+++ route/v1/attachRouter.go
@@ -2,11 +2,12 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
+	"main.go/app/v1/attach/controller"
 )
 
 func AttachRouter(route *gin.RouterGroup) {
 	route.Any("/", func(context *gin.Context) {
 		context.String(0, route.BasePath())
 	})
-
+	controller.IndexController(route.Group("attach"))
 }
